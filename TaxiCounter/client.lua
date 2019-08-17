00f3aa736e8d72b3ad353ead7904644a359c45fb
SuperChocolatine20 <45998396+SuperChocolatine20@users.noreply.github.com>
2019-08-17 21:29:44 +0200

    Add files via upload

---
+++ TaxiCounter/client.lua
@@ -0,0 +1,114 @@
+--Prix du kilomètre
+local perKilometer = 10
+--Prix minimum
+local  min = 5;
+
+--prix de l'heure le jour (2 mins IRL)
+local perGTAHourDay = 3
+--prix de l'heure la nuit (2 mins IRL)
+local perGTAHourNight = 5
+
+--Vitesse moyenne
+local averageV = 0
+--Adition des vitesses
+local total = 0
+--Prix
+local price = 0
+
+--Formule jour/nuit
+local night = false
+--Calcul le prix ?
+local started = false
+--Repère temps 1/2 sec
+local time = 0
+
+
+RegisterNetEvent('taxi-counter:ChooseNight')
+AddEventHandler('taxi-counter:ChooseNight', function ()
+    night = true
+end)
+
+RegisterNetEvent('taxi-counter:ChooseDay')
+AddEventHandler('taxi-counter:ChooseDay', function ()
+    night = false
+end)
+
+RegisterNetEvent('taxi-counter:Start')
+AddEventHandler('taxi-counter:Start', function()
+    Clear()
+    started = true
+    Calcul()
+end)
+
+
+
+RegisterNetEvent('taxi-counter:Clear')
+AddEventHandler('taxi-counter:Clear', function()
+    total = 0
+    time = 0
+    price = 0
+end)
+
+function Clear()
+    total = 0
+    time = 0
+    price = 0
+    started = false
+end
+
+RegisterNetEvent('taxi-counter:Stop')
+AddEventHandler('taxi-counter:Stop', function()
+        started = false
+end)
+
+RegisterNetEvent('taxi-counter:Stop')
+AddEventHandler('taxi-counter:Pay', function()
+        --Votre script de facturation
+end)
+
+function Calcul()
+    while true do
+        Citizen.Wait(50)
+        local player = GetPlayerPed(-1)
+        local plVehicle = GetLastDrivenVehicle(player)
+        if not started then
+                return
+        end
+        
+        --Gérer le temps
+        time = time + 1
+        total = total + GetEntitySpeed(plVehicle) * 3.6
+        averageV = total / time
+
+        -- V x T
+        local distance = averageV * time / 3600 / 20
+
+        if night then
+                price = (min + perKilometer * distance + (time / 600 * perGTAHourNight))
+        elseif not night then
+                price = (min + perKilometer * distance + (time / 600 * perGTAHourDay))
+        end
+
+        local message = "~b~ Le prix de la course est de: ~g~" .. tostring(round(price)) .. " $."
+
+        ShowSubtitle(message, 51)
+      
+    end
+end
+
+function round(n)
+    return n % 1 >= 0.5 and math.ceil(n) or math.floor(n)
+end
+
+
+function ShowNotification(text)
+    SetNotificationTextEntry("STRING")
+    AddTextComponentString(text)
+    DrawNotification(false, true)
+end
+
+function ShowSubtitle(text, ms)  
+    BeginTextCommandPrint("STRING") 
+    AddTextComponentSubstringPlayerName(text)
+    EndTextCommandPrint(ms, 1)
+end
